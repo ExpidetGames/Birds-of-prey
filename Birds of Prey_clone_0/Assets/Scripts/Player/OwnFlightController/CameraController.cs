dc716a5cabc1a371bca9e5412fcc88e8b0e76a70
winzig3r <helmers.vincent@gmail.com>
2022-07-03 15:55:05 +0200

    Camera Controller

--- Birds of Prey_clone_0/Assets/Scripts/Player/OwnFlightController/CameraController.cs
+++ Birds of Prey_clone_0/Assets/Scripts/Player/OwnFlightController/CameraController.cs
@@ -7,6 +7,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private GameObject viewPoint;
     [SerializeField] private Camera playerCamera;
     [SerializeField] private float smoothSpeed;
+    public float plerp = .01f;
+    public float rlerp = .02f;
+    Quaternion rotGoal;
+    Vector3 lookPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,10 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, new Vector3(viewPoint.transform.position.x, viewPoint.transform.position.y + 2, viewPoint.transform.position.z -10), smoothSpeed);
-        playerCamera.transform.LookAt(viewPoint.transform.position);
+        lookPoint = new Vector3(viewPoint.transform.position.x,viewPoint.transform.position.y +2,viewPoint.transform.position.z - 10);
+        //playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, new Vector3(viewPoint.transform.position.x, viewPoint.transform.position.y + 2, viewPoint.transform.position.z -10), smoothSpeed);
+        //playerCamera.transform.LookAt(viewPoint.transform.position);
+        playerCamera.transform.position = Vector3.Lerp(playerCamera.transform.position, lookPoint, plerp);
+        playerCamera.transform.rotation = Quaternion.Lerp(playerCamera.transform.rotation, viewPoint.transform.rotation, plerp);
     }
 }
