bd1f2832f49dd39f17ce1d94ae45dd9f1bd4f224
Schacki_Jr <luca.schackmann@gmail.com>
2022-07-03 17:02:42 +0200

    flightcontroller perfection

--- Birds of Prey/Assets/Scripts/Player/OwnFlightController/FlightController.cs
+++ Birds of Prey/Assets/Scripts/Player/OwnFlightController/FlightController.cs
@@ -6,7 +6,7 @@ public class FlightController : MonoBehaviour
 {
   public float speed = .05f;
   float zRotation;
-  public float rotationResetTinmer;
+  public float zRotationLerpSpeed;
   public Rigidbody rb;
 
   // Use this for initialization
@@ -19,21 +19,23 @@ public class FlightController : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
-
+    Debug.Log(Input.GetAxis("Mouse X"));
     if(Input.GetAxis("Mouse X") == 0){
-      rotationResetTinmer = rotationResetTinmer + 1 * Time.deltaTime;
+      transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y,zRotationLerpSpeed),2f * Time.deltaTime);
     }
     else{
        zRotation = (Input.GetAxis("Mouse X") + transform.rotation.x) * 40;
-       rotationResetTinmer = 0;
     }
-    // if(rotationResetTinmer >3){
-    //   transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity,.2f);
-    // }
-   
-
+     float rotation;
+     if(transform.rotation.eulerAngles.z > 180){
+      rotation =360 - transform.rotation.eulerAngles.z;
+     }
+     else{
+      rotation = -transform.rotation.eulerAngles.z;
+     }
+    Debug.Log(transform.rotation.eulerAngles.z);
     transform.Rotate(Input.GetAxis("Vertical") , Input.GetAxis("Horizontal"), -zRotation * Time.deltaTime);
-    
+    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + rotation / 40, transform.rotation.eulerAngles.z);
     transform.position += transform.forward * Time.deltaTime * speed;
     //rb.AddForce(this.gameObject.transform.TransformDirection(Vector3.forward) * speed * Time.deltaTime);
     
